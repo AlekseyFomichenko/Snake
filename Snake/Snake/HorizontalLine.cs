73d1f1c9624803f48f058aaeb854accb0d29cc74
Aleksey <fomichaaf.95@gmail.com>
2023-05-02 12:18:21 +0300

    class HorizontalLine

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class HorizontalLine
+    {
+        List<Point> points;
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+            points = new List<Point>();
+            for (int x = xLeft; x < xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                points.Add(p);
+            }
+        }
+        public void Draw()
+        {
+            foreach (Point p in points) { p.Draw(); }
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -18,47 +18,11 @@ namespace Snake
 
             Point p2 = new Point(7, 4, '#');
             p2.Draw();
-            /*
-            int x = 1;
-            Func1(x);
-            Console.WriteLine("Call func. x = " + x);
 
-            x = 1;
-            Func2(x);
-            Console.WriteLine("Call func2 x = " + x);
-
-            x = 1;
-            Func3(x);
-            Console.WriteLine("Call func3 x = " + x);
-
-            Point p1 = new Point(1, 3, '*');
-            Move(p1, 10, 10);
-            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
-
-            Point p2 = new Point(4, 5, '#');
-            p1 = p2;
-            p2.x = 8;
-            p2.y = 8;
-            Console.WriteLine("p1 = p2. p1.x = " + p1.x + ", p1.y = " + p1.y + "; p2.x = " + p2.x + ", p2.y = " + p2.y);
-
-            p1 = new Point(1, 3, '*');
-            Update(p1);
-            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
-            */
-            List<Point> points = new List<Point>();
-            points.Add(p1);
-            points.Add(p2);
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Draw();
         }
-        /*
-        public static void Func1(int value) { }
-        public static void Func2(int value) { value++; }
-        public static void Func3(int X) { X++; }
-        public static void Move(Point p, int dx, int dy)
-        {
-            p.x += dx; p.y += dy;
-        }
-        public static void Update(Point p) { p = new Point(); }
-        */
+        
 
     }
 }
